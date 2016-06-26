a870af14d534e9bafbab65fd82446ef5d32764aa
Matthew Nho <falseym31@gmail.com>
2016-06-27 08:51:39 +0900

    Add test function parsing data aligned in core bit

--- src/main/resources/org/somecpp/NativeLibrary.h
+++ src/main/resources/org/somecpp/NativeLibrary.h
@@ -1,10 +1,24 @@
 #include <string>
 
 namespace NativeLibrary {
+    struct FRAME {
+        int age;
+        int rate;
+    };
+
     class NativeClass {
         public:
             const std::string& get_property() { return property; }
             void set_property(const std::string& property) { this->property = property; }
             std::string property;
+
+            const struct FRAME* parse(char *data, int len) { 
+                /* struct FRAME *pFrame = new struct FRAME; */
+                /* pFrame->age = 0; */
+
+                // Assume that data size is identical with sizeOf(FRAME)
+                struct FRAME *pFrame = (struct FRAME *)data;
+                return pFrame; 
+            }
     };
 }
